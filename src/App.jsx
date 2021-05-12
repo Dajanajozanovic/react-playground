a8cc74dfed0f06974247f32e69e709dc214eceae
Dajana Jozanovic <dajana.jozanovic@t-online.de>
2021-05-12 13:50:13 +0200

    create todoList

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
-import * as React from 'react'
-import Card from './Card'
-
-const cards = [
-  {
-    title: 'Frage',
-    questionText: 'Wie geht es Dir?',
-    tags: ['Prima', 'super', 'bestens'],
-    id: 1,
-  },
-  {
-    title: 'Frage',
-    questionText: 'Was machst Du?',
-    tags: ['Codes schreiben'],
-    isAnswerVisible: true,
-    id: 2,
-  },
-]
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import './App.css'
 
 export default () => {
+  const [todos, setTodos] = useState([])
   return (
     <div className="App">
-      {cards.map(({ id, title, questionText, tags, isAnswerVisible }) => (
-        <Card
-          key={id}
-          title={title}
-          isAnswerVisible={isAnswerVisible}
-          questionText={questionText}
-          tags={tags}
-        />
-      ))}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Add todo:
+          <input name="todo" type="text" />
+        </label>
+        <button>Add</button>
+      </form>
+      <ul>
+        {todos.map(({ text, isDone, id }) => (
+          <li key={id}>
+            {text} {isDone && '✅'}
+          </li>
+        ))}
+      </ul>
     </div>
   )
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const form = event.target
+    const input = form.elements.todo
+    // spread operator `...todos`
+    const newTodo = { text: input.value, isDone: false, id: uuidv4() }
+    setTodos([newTodo, ...todos])
+    form.reset()
+    input.focus()
+  }
 }
